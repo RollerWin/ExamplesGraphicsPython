e4f4a74b1a42d0b943389241ac8690d7a25c5f36
RollerWin <rinatyych@mail.ru>
2023-10-03 01:22:45 +0300

    changed rendering method

--- priceChartBar.py
+++ priceChartBar.py
@@ -1,44 +1,87 @@
 import turtle
 import random
 
-def drawbar(height, color):
-    brush.fillcolor(color)
-    
-    brush.begin_fill()
-    brush.left(90)
-    brush.forward(height)
-    brush.write(str(height))
-    brush.right(90)
-    brush.forward(20)
-    brush.right(90)
-    brush.forward(height)
-    brush.left(90)
-    brush.end_fill()
+default_price = 150
+max_value = 180
+min_value = 120
+current_price = random.randint(min_value, max_value)
+epsilon = 20
+step = 15
+is_up = 0
 
-default_price = 300
-max_value = 500
-min_value = 20
-number_of_values = 25
-
-values = [0] * number_of_values
-
-for i in range(number_of_values):
-    values[i] = random.randint(min_value, max_value)
-
-border_length = 10
-maxheight = max(values)
-numbers = len(values)
 good_price_color = "green"
 bad_price_color = "red"
 
 screen = turtle.Screen()
-screen.setworldcoordinates(0 - border_length, 0 - border_length, 40 * numbers + border_length, maxheight+border_length)
 brush = turtle.Turtle()
 
-for i in range(numbers):
-    if values[i] >= default_price:
-        drawbar(values[i], good_price_color)
+brush.speed(10)
+
+brush.penup()
+brush.goto(-350, -300)  # Перемещаемся в начальную точку для оси времени
+brush.pendown()
+brush.forward(500)  # Рисуем горизонтальную линию оси времени
+
+# Рисуем ось стоимости
+brush.penup()
+brush.goto(-350, -300)  # Перемещаемся в начальную точку для оси стоимости
+brush.pendown()
+brush.left(90)  # Поворачиваем черепаху на 90 градусов (вертикальная линия)
+brush.forward(500)  # Рисуем вертикальную линию оси стоимости
+
+brush.penup()
+brush.goto(-350,-167)
+brush.right(90)
+brush.pendown()
+
+for _ in range(25):
+    brush.forward(10)
+    brush.penup()
+    brush.forward(10)
+    brush.pendown()
+
+# Подписываем оси
+brush.penup()
+brush.goto(-400, 0)  # Перемещаемся для подписи оси времени
+brush.write("Стоимость")
+
+brush.goto(0, -320)  # Перемещаемся для подписи оси стоимости
+brush.write("Время")
+
+
+brush.goto(-350, -300)
+brush.pendown()
+
+brush.width(3)
+brush.left(45)
+
+def drawbar(step, color, current_price):
+    brush.color(color)
+    brush.forward(step)
+    brush.write(str(current_price))
+
+
+if(current_price > default_price):
+    drawbar(current_price + 40, good_price_color, current_price)
+else:
+    drawbar(current_price + 40, bad_price_color, current_price)
+
+while 10:
+
+    previous_price = current_price
+    current_price = random.randint(current_price - epsilon, current_price + epsilon)
+    step = abs(current_price - previous_price)
+
+    if previous_price > current_price and not is_up:
+        brush.right(90)
+        is_up = 1
+    elif previous_price < current_price and is_up:
+        brush.left(90)
+        is_up = 0
+
+    if current_price >= default_price:
+        drawbar(step, good_price_color, current_price)
     else:
-        drawbar(values[i], bad_price_color)
+        drawbar(step, bad_price_color, current_price)
 
 screen.exitonclick()
